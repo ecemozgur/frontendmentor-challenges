cd47bd9f4f177e29e0f5457cbabe8ff8f8db9718
ecemozgur <sayarecem93@gmail.com>
2024-10-20 09:08:30 +0300

    add new project

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,11 @@
+// const share = document.getElementById("share");
+
+// function toggleShare() {
+//   console.log("sdfsf");
+// }
+
+// share.addEventListener("click", toggleShare);
+
+function share() {
+  document.getElementById("share").style.display = "block";
+}
